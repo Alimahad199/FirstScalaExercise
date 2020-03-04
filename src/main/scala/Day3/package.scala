9eb5273d7a5bfa48e69acbe31180a7849ac7ff0c
Alimahad199 <alimahad.eng@gmail.com>
2020-03-04 15:09:25 +0000

    Added Strings

---
+++ src/main/scala/Day3/package.scala
@@ -0,0 +1,12 @@
+package Day3
+object Strings extends App {
+
+  def lastChar( word : String, amount : Int) : String = {
+
+    word.takeRight(amount)
+
+  }
+    println( lastChar ("Alimahad" , 4))
+
+
+  }
