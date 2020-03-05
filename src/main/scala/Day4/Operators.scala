81ba54433a60a3c2c8c8317c01f337632cc2d0d3
Alimahad199 <alimahad.eng@gmail.com>
2020-03-05 11:51:12 +0000

    Added Operators with comments

---
+++ src/main/scala/Day4/Operators.scala
@@ -0,0 +1,19 @@
+import scala.io.StdIn._
+
+package Day4{
+
+object Operators extends App {
+
+// Create a method that takes in two integers and returns and integer
+  def twoIntInput (numOne : Int, numTwo : Int ) : Int = {
+
+// Within the body of the method specify what exactly you want to with the integers
+    numOne + numTwo
+
+
+  }
+  // Within the println call the method (twoIntInput) and pass through the integers you want to add
+        println(twoIntInput(10,9))
+
+      }
+}
